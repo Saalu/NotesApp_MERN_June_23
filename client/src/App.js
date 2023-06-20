c2b4580cddc555880b357a96bf415d9c9226d6bd
saalu <saaluissaka@gmail.com>
2023-06-20 06:27:37 +0000

    Login UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./components/Login";
+import Notes from "./components/Notes";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [isLogin, setIsLogin] = useState(false);
+  return <div className="App">{isLogin ? <Notes /> : <Login />}</div>;
 }
 
 export default App;
