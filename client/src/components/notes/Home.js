d9497ca51e4aa5ecd3e3c419230d068beb0db2b2
saalu <saaluissaka@gmail.com>
2023-07-04 14:15:37 +0000

    fetch delete logic

--- client/src/components/notes/Home.js
+++ client/src/components/notes/Home.js
@@ -1,24 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { format } from "timeago.js";
+import axios from "axios";
 
 function Home() {
+  const [notes, setNotes] = useState([]);
+  const [token, setToken] = useState("");
+
+  const getNotes = async (token) => {
+    const res = await axios.get("api/notes", {
+      headers: { Authorization: token },
+    });
+    setNotes(res.data);
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem("tokenStore");
+    setToken(token);
+    if (token) {
+      getNotes(token);
+    }
+  }, []);
+
+  const deleteNote = async (id) => {
+    console.log(id);
+    try {
+      if (token) {
+        await axios.delete(`api/notes/${id}`, {
+          headers: { Authorization: token },
+        });
+        getNotes(token);
+      }
+    } catch (er) {
+      window.location.href = "/";
+    }
+  };
+
   return (
     <div className="note-wrapper">
-      <div className="card">
-        <h4 className="title">Note Title</h4>
-        <div className="text-wrapper">
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. In ipsum
-            quidem a fuga quae cum.
-          </p>
+      {notes.map((note) => (
+        <div className="card" key={note._id}>
+          <h4 className="title">{note.title}</h4>
+          <div className="text-wrapper">
+            <p>{note.content}</p>
+          </div>
+          <p className="date">{format(note.date)}</p>
+          <div className="card-footer">
+            {note.name}
+            <Link to={`edit/${note._id}`}>Edit</Link>
+          </div>
+          <button className="close" onClick={() => deleteNote(note._id)}>
+            X
+          </button>
         </div>
-        <p className="date">Note Date</p>
-        <div className="card-footer">
-          User Name
-          <Link to="/">Edit</Link>
-        </div>
-        <button className="close">X</button>
-      </div>
+      ))}
     </div>
   );
 }
